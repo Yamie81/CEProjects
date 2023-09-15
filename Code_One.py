a3de8aa6eaa98891afda8b5b29169cd41020cdb0
Yamie81 <ambali.my@gmail.com>
2023-09-15 14:39:30 +0200

    Code added

---
+++ Code_One.py
@@ -0,0 +1,13 @@
+print("1. Add")
+print ("2. Subtract")
+print ("3. Multiplication")
+print ("4. Exit")
+User_Choice = int(input("Enter your Option :"))
+if User_choice <1 or User_choice > 4 
+    print ("Invalid Option")
+else
+    num1 = input ("Enter First Number :")
+    num2 = input ("Enter Second number :")  
+    Total = int(num1)+int(num2)
+    print Total
+                
